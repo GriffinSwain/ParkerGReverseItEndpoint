9e8e70813c7b04e302a804390c4a4e0d3ed97fcd
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2022-10-27 10:32:56 -0700

    first commit

---
+++ Controllers/ReverseItController.cs
@@ -0,0 +1,30 @@
+//Griffin Parker
+//October 26, 2022
+//Reverse It - Endpoint
+//This program gives the user information on how to run the program, takes in a user's input through the URL and 
+//Peer Review: Samuel Laguna: I ran the code it works good. I reverses how it is supposed to do. All the inputs work correctly. this code is all good.
+//Instructions: https://localhost:xxxx/reverseit
+//Full program: https://localhost:xxxx/reverseit/reverse/{input}
+
+using Microsoft.AspNetCore.Mvc;
+namespace ParkerGReverseItEndpoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ReverseItController : ControllerBase
+{
+public string Instructions()
+{
+    return " To run this program, type \n\n https://localhost:xxxx/reverseit/reverse/{input} \n\n Be sure to change the 'xxxx' to where your system is hosting it locally, and change {input} to what you want to be reversed";
+}
+
+    [HttpGet]
+    [Route("Reverse/{input}")]
+    public string ReverseIt(string input)
+    {
+    string reverse = ""; 
+    for(int i = input.Length - 1; i >= 0; i--) reverse = reverse + input[i];
+    return $"{input} backwards is {reverse}";
+    }
+}
