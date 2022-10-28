5e7a89b711eb2ab5fe4f0413aa9a1da12bc6cef0
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2022-10-27 18:25:17 -0700

    Update ReverseItController.cs

--- Controllers/ReverseItController.cs
+++ Controllers/ReverseItController.cs
@@ -1,7 +1,7 @@
 //Griffin Parker
 //October 26, 2022
 //Reverse It - Endpoint
-//This program gives the user information on how to run the program, takes in a user's input through the URL and 
+//This program gives the user information on how to run the program, takes in a user's input through the URL and reverse the characters in their input
 //Peer Review: Samuel Laguna: I ran the code it works good. I reverses how it is supposed to do. All the inputs work correctly. this code is all good.
 //Instructions: https://localhost:xxxx/reverseit
 //Full program: https://localhost:xxxx/reverseit/reverse/{input}
